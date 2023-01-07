3ab55c10b4bd05d8bba6588b5d0fbcf90fd740d5
KevinDavila <kevindavila.developer@gmail.com>
2023-01-06 18:17:02 -0600

    initial

---
+++ youtump3.py
@@ -0,0 +1,16 @@
+from pytube import YouTube
+
+
+def Download(link):
+    ytObject = YouTube(link)
+    ytObject = ytObject.streams.filter(progressive=True, file_extension='mp4')
+    print(ytObject)
+    try:
+        ytObject.download()
+    except:
+        print("ocurrio un error")
+    print("La descarga se ah Completado")
+
+
+link = input("ingresa link de YT: ")
+Download(link)
